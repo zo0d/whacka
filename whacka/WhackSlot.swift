a0db3af4f042115ce994020fff0086c30ce49a2a
BJ <bj@zo0d.hitronhub.home>
2019-05-18 22:16:03 -0400

    Init

---
+++ whacka/WhackSlot.swift
@@ -0,0 +1,35 @@
+//
+//  WhackSlot.swift
+//  whacka
+//
+//  Created by BJ on 2019-05-18.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+
+    }
+}
